d9885c668b965c21a0ab761253a1ebf4ebee6269
Muhammad Usman <usman.mzahoor@emumba.com>
2025-06-05 10:12:24 +0500

    feat: Add requests logger

--- server/src/config/server.ts
+++ server/src/config/server.ts
@@ -1,7 +1,9 @@
 import express, { Express } from 'express';
 import helmet from 'helmet';
+import morgan from 'morgan';
 
 export const setupServer = (app: Express) => {
   app.use(helmet());
   app.use(express.json());
+  app.use(morgan('combined'));
 };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
 import { app } from './app';
 import { env } from './config';
+import { logger } from './utils';
 
 const server = app.listen(env.PORT, () => {
   console.log('Server lsitening on port', env.PORT);
 });
 
 process.on('unhandledRejection', (error: Error) => {
-  console.log('unhandledRejection', error);
+  logger.error(`unhandledRejection: Error (${error.message}) `);
   server.close(() => process.exit(1));
 });
