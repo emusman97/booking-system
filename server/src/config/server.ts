0cfb8d42093def846d433eae34645377f988841f
Muhammad Usman <usman.mzahoor@emumba.com>
2025-06-17 15:52:46 +0500

    fix: Fix cors error

--- server/src/config/server.ts
+++ server/src/config/server.ts
@@ -1,9 +1,23 @@
 import express, { Express } from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import cors from 'cors';
 
 export const setupServer = (app: Express) => {
-  app.use(helmet());
+  app.use(
+    cors({
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    })
+  );
+  app.use(
+    helmet({
+      referrerPolicy: { policy: 'origin-when-cross-origin' },
+    })
+  );
   app.use(express.json());
   app.use(morgan('combined'));
 };
